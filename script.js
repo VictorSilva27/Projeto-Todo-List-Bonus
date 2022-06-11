f7cb5455f3dafbf46b438bbc283b64852ca456d9
VictorSilva27 <victoradaosilva2@gmail.com>
2022-06-11 13:40:13 -0300

    Requisito 10 e 12

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
 const tasksTxt = document.getElementById('texto-tarefa');
 const buttonCreateTasks = document.querySelector('#criar-tarefa');
 const ol = document.querySelector('#lista-tarefas');
+const clearAll = document.querySelector('#apaga-tudo');
+const saveAll = document.querySelector('#salvar-tarefas');
+
+const salvarLista = () => localStorage.setItem('list', ol.innerHTML);
+
+const getList = () => localStorage.getItem('list');
+
+saveAll.addEventListener('click', salvarLista);
+
+const clearOl = () => {
+  ol.innerHTML = '';
+};
+
+clearAll.addEventListener('click', clearOl);
 
 const createLiWithText = () => {
   const li = document.createElement('li');
-  li.className = 'tarefas';
   li.innerHTML = tasksTxt.value;
   li.addEventListener('click', () => {
     li.style.backgroundColor = 'gray';
+  });
+  li.addEventListener('dblclick', () => {
+    li.className = 'completed';
+    li.style.textDecoration = 'line-through solid black';
   });
   ol.appendChild(li);
   tasksTxt.value = '';
 };
 
 buttonCreateTasks.addEventListener('click', createLiWithText);
+
+window.onload = () => {
+  ol.innerHTML = getList();
+};
